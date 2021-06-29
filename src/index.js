3e5003e45010bd9e8d82e96fe0858a1bba10d6ff
azusa29 <71913135+azusa29@users.noreply.github.com>
2021-06-29 12:40:09 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-import "./styles.css";
+/**
+ * const, letの変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は際宣言可能
+// var val1 = "var変数を際宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// letは再宣言不可能
+// let val2 = "ののんおん";
+
+// const val4 = {
+//   name: "あず",
+//   age: 30
+// };
+
+// val4.name = "azusa";
+// console.log(val4);
+
+/**
+ * テンプレート文字列
+ */
+
+// const name = "azu";
+// const age = 28;
+
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// // テンプレート文字列
+// const message2 = `私の名前は${name}です。${age}歳です。`
+// console.log(message2);
+
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (str) => str;
+console.log(func3("returnいらいない"));
+
+const func4 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func4(10, 20));
